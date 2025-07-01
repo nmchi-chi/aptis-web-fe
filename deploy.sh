7a773a86c703129940fab4b97ebc5e15e7d99d78
chi <23025055@vnu.edu.vn>
2025-07-01 22:20:12 +0700

    fix bug

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-IMAGE_NAME=aptis-fe
-CONTAINER_NAME=aptis-fe-container
-PORT=80
+rm -rf node_modules package-lock.json
+npm install --legacy-peer-deps
+npm ci
 
-docker stop $CONTAINER_NAME 2>/dev/null || true
-docker rm $CONTAINER_NAME 2>/dev/null || true
+npm run build
 
-docker build -t $IMAGE_NAME .
-
-docker run -d --name $CONTAINER_NAME -p $PORT:80 $IMAGE_NAME
-
-echo "Deployment complete. App is running at http://localhost:$PORT"
+cp -r /root/aptis-web-fe/build/* /var/www/react-app/
